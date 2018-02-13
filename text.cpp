d0c46d1d59cd8c1dc25b54bcc120fbcb590cd75a
DaveThePixelPirate <DaveThePixelPirate@users.noreply.github.com>
2018-02-13 00:32:38 -0400

    Added a text.cpp file to output the variables to a textfile called abc.txt

---
+++ text.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+	ofstream outputFile("abc.txt");
+	if(outputFile){
+   cout<<a<<" "<<b<<" "<<c;//display data to console (i.e. as formatted chars)
+	outputFile<<a <<" "<<b<<" "<<c;
+   cout<<endl<<endl;
+   }
+   return 0;
+   
+}
